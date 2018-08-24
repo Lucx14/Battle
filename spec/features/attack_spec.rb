173082a6a31df859865147b19e7557c54d486d72
Lucien Najev <luciennajev@hotmail.com>
2018-08-24 11:57:01 +0100

    refactoring

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,11 @@
+
+feature 'Attacking' do
+  # As Player 1,
+  # So I can win a game of Battle,
+  # I want to attack Player 2, and I want to get a confirmation
+  scenario 'attack Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Pikachu attacked Piplup'
+  end
+end
